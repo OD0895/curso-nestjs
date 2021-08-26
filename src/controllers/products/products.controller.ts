9d5caf3800a3b5b3b3f8d5c56c81c2b56c8ccabe
Osvaldo Amador Salinas <osvaldodaniel0895@gmail.com>
2021-08-26 12:30:03 -0500

    :sparkles: Agregando un servicio y agregando NotFoundException para los errores

--- src/controllers/products/products.controller.ts
+++ src/controllers/products/products.controller.ts
@@ -1,16 +1,35 @@
-import { Controller, Get, Query, Param, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Query,
+  Param,
+  Post,
+  Delete,
+  Put,
+  Body,
+  HttpStatus,
+  HttpCode,
+} from '@nestjs/common';
+
+import { Response } from 'express';
+
+import { ProductsService } from './../../services/products/products.service';
 
 @Controller('products')
 export class ProductsController {
+  //esto es la inyecccion de la dependencia crea una instancia de la dependencia
+  constructor(private productsService: ProductsService) {}
+
   @Get()
   getProducts(
     @Query('limit') limit = 100,
     @Query('offset') offset = 0,
     @Query('brand') brand: string,
   ) {
-    return {
-      message: `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`,
-    };
+    // return {
+    //   message: `products limit=> ${limit} offset=> ${offset} brand=> ${brand}`,
+    // };
+    return this.productsService.findAll();
   }
 
   @Get('filter')
@@ -19,15 +38,30 @@ export class ProductsController {
   }
 
   @Get(':productId')
+  @HttpCode(HttpStatus.ACCEPTED)
   getOne(@Param('productId') productId: string) {
-    return `product ${productId}`;
+    // response.status(200).send({
+    //   message: `product ${productId}`,
+    // });
+    return this.productsService.findOne(+productId);
   }
 
   @Post()
   create(@Body() payload: any) {
-    return {
-      message: 'accion de crear',
-      payload,
-    };
+    // return {
+    //   message: 'accion de crear',
+    //   payload,
+    // };
+    return this.productsService.create(payload);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() payload: any) {
+    return this.productsService.update(+id, payload);
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return this.productsService.remove(+id);
   }
 }
